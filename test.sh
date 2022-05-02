192b399b228164dba1d53a39bd3accc33bd57371
Wertik <jregvx@gmail.com>
2022-05-02 19:42:56 +0200

    Edit .gitignore

--- test.sh
+++ test.sh
@@ -14,4 +14,10 @@ do
     ./proj2 3 5 100 100
 done
 
+for i in `seq 1 $1`
+do
+    echo "TEST (ODD) NUMBER $i"
+    ./proj2 5 3 100 100
+done
+
 echo "Done... if you see this message. No deadlocks happened."
